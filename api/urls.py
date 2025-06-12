566a0a90e0b125d0988ea849d269dfaa3ffcff7f
Micha Hulsbosch <michahulsbosch@gmail.com>
2025-06-12 13:46:59 +0200

    feat: Login setup
    See https://tomdekan.com/articles/vue-auth

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from .views import drop_classify_view, process_view, send_manuscripts_view, transform_view
-
+from . import views
 
 
 urlpatterns = [
-    path('drop-classify', drop_classify_view, name='drop_classify'),
-    path('process', process_view, name='process'),
-    path('send_manuscripts', send_manuscripts_view, name='send_manuscripts'),
-    path('transform', transform_view, name='transform'),
+    path('drop-classify', views.drop_classify_view, name='drop_classify'),
+    path('process', views.process_view, name='process'),
+    path('send_manuscripts', views.send_manuscripts_view, name='send_manuscripts'),
+    path('transform', views.transform_view, name='transform'),
+    path('set-csrf-token', views.set_csrf_token, name='set_csrf_token'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('user', views.user, name='user'),
+    path('register', views.register, name='register'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,7 +10,64 @@ from api.paths.drop_classify import drop_classify
 from api.paths.property_structuring import send_manuscipts
 from api.paths.rdfData import transform_data_into_rdf
 from api.models import Activity
+from django.views.decorators.csrf import ensure_csrf_cookie
+import json
+from django.contrib.auth import authenticate, login, logout
+from .forms import CreateUserForm
 
+
+@ensure_csrf_cookie
+@require_http_methods(['GET'])
+def set_csrf_token(request):
+    """
+    We set the CSRF cookie on the frontend.
+    """
+    return JsonResponse({'message': 'CSRF cookie set'})
+
+@require_http_methods(['POST'])
+def login_view(request):
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+        email = data['email']
+        password = data['password']
+    except json.JSONDecodeError:
+        return JsonResponse(
+            {'success': False, 'message': 'Invalid JSON'}, status=400
+        )
+
+    user = authenticate(request, username=email, password=password)
+
+    if user:
+        login(request, user)
+        return JsonResponse({'success': True})
+    return JsonResponse(
+        {'success': False, 'message': 'Invalid credentials'}, status=401
+    )
+
+def logout_view(request):
+    logout(request)
+    return JsonResponse({'message': 'Logged out'})
+
+@require_http_methods(['GET'])
+def user(request):
+    if request.user.is_authenticated:
+        return JsonResponse(
+            {'username': request.user.username, 'email': request.user.email}
+        )
+    return JsonResponse(
+        {'message': 'Not logged in'}, status=401
+    )
+
+@require_http_methods(['POST'])
+def register(request):
+    data = json.loads(request.body.decode('utf-8'))
+    form = CreateUserForm(data)
+    if form.is_valid():
+        form.save()
+        return JsonResponse({'success': 'User registered successfully'}, status=201)
+    else:
+        errors = form.errors.as_json()
+        return JsonResponse({'error': errors}, status=400)
 
 @require_http_methods(["POST"])
 @csrf_exempt
